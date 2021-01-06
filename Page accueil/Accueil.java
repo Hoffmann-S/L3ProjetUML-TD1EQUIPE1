a6402c704babfd3868966029b00705a4562593bf
lydia soltani <lydia.soltani@alumni.univ-avignon.fr>
2021-01-06 13:54:20 +0100

    Page d'accueil version2

--- Page accueil/Accueil.java
+++ Page accueil/Accueil.java
@@ -10,7 +10,9 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-
+import java.awt.*;
+import javax.swing.JFrame;
+import java.util.Scanner;
 public class Accueil {
 
     Label label1= new Label("Jeu : Quand on arrive en ville");
@@ -21,3 +23,31 @@ public class Accueil {
     Button button1= new Button("Commencer à jouer ");
     button1.setStyle("-fx-background-color: #FFFFFF");
 }
+
+    public static void choixUtilisateur()
+    {
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        String Pseudo;
+        System.out.println("Choissisez pseudo:");
+        Pseudo = myObj.nextLine();
+        System.out.println("Choissisez personnage:");
+        switch(myObj.nextInt())
+        {
+            case 0:
+                Plateau.p = new Standard(Pseudo);
+                break;
+            case 1:
+                Plateau.p = new Hippie(Pseudo);
+                break;
+            case 2:
+                Plateau.p = new HommePresse(Pseudo);
+                break;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        choixUtilisateur();
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Taille du plateau:");
+        int taillePlateau = myObj.nextInt();
